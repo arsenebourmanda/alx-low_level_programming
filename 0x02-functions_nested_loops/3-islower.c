da4183761e49d2ed92af469fad0880a6ade8a8ac
koumnomani bourmanda <arsenebourmanda@gmail.com>
2023-06-20 16:45:48 -0700

    creates 3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _islower - Write a function that checks for lowercase character.
+ *
+ * Return: 1 if c is lowercase 0 otherwise
+ */
+int _islower(int c)
+{
+if ((c >= 61) && (c <= 122))
+_putchar('1');
+else
+_putchar('0');
+}
+
