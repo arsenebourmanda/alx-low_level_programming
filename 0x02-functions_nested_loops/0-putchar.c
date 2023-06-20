5a6a902f428d8e874ca1d60de8b58a77f31ebcba
koumnomani bourmanda <arsenebourmanda@gmail.com>
2023-06-20 14:51:39 -0700

    creates 0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+
+/**
+ * main -Code begine here.
+ *
+ * Return: Always 0
+ */
+int _putchar()
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+	return (0);
+}
