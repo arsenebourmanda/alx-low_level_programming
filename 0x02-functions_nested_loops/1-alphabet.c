79557f4aa9c6aeeb63e609a641325a2b19616f71
koumnomani bourmanda <arsenebourmanda@gmail.com>
2023-06-20 15:35:26 -0700

    creates 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int i;
+
+    i = 97;
+    while(i<=122)
+    {
+       _ putchar(i);
+        i++;
+    }
+    _putchar('\n');
+    return (0);
+}
