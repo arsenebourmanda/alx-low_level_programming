81ecda1184cbbbdbd51a58f1829a04995925ef3d
koumnomani bourmanda <arsenebourmanda@gmail.com>
2023-06-20 16:32:56 -0700

    creates 4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _islower - Write a function that checks for lowercase character.
+ *
+ * Return: Always 0.
+ */
+int _islower(int c)
+{
+if ((c >= 61) && (c <= 122))
+-putchar('1');
+else
+_putchar('0');
+}
