af81d27c486981b768d78139ef32f0401e30836d
koumnomani bourmanda <arsenebourmanda@gmail.com>
2023-06-20 17:53:05 -0700

    creates 4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,19 +1,19 @@
 #include "main.h"
 
 /**
- * int _isalpha - Write a function that checks for lowercase character.
+ * _isalpha - Write a function that checks for lowercase character.
  * @c: Number to pass to _islower function
  * Return: Always 0.
  */
 int _isalpha(int c)
 {
-for(c = 65; c <= 122; c++)
+for (c = 65; c <= 122; c++)
 {
 if ((c >= 97) && (c <= 122))
 {
 return (1);
 }
-else if ((c >= 65) && (c <= 90))
+else if ((c >= 65) && (c <= 97))
 {
 return (1);
 }
