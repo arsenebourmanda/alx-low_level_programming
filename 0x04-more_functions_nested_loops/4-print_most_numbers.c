789e50702ed1ab249e396b9b601275f70b6fed98
koumnomani bourmanda <arsenebourmanda@gmail.com>
2023-06-22 17:12:16 -0700

    creates 4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+* print_most_numbers - prints the numbers, from 0 to 9, except 2 and 4
+*
+* Return: Always 0.
+*/
+void print_most_numbers(void)
+{
+int i;
+
+for (i = 48; i < 58; i++)
+{
+if (i != 50)
+{
+if (i != 52)
+{
+_putchar(i);
+}
+}
+}
+_putchar('\n');
+}
