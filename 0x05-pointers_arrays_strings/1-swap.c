6860631b1fc0196a140ca3b3ec9f69d712328db4
koumnomani bourmanda <arsenebourmanda@gmail.com>
2023-06-26 16:17:13 -0700

    creates 1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * swap_int - function that swaps the values of two integers.
+ * @a: Pointer
+ * @b:Pointer
+ * Return: Void.
+ */
+void swap_int(int *a, int *b)
+{
+int x, y;
+a = &x;
+b = &y;
+}
