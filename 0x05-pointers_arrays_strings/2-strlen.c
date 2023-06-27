1e5aeaf1dc5dd408e55d99ec3c7daf0fa87ffe19
koumnomani bourmanda <arsenebourmanda@gmail.com>
2023-06-26 18:46:43 -0700

    creates 2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -7,11 +7,11 @@ Task 2
  */
 int _strlen(char *s)
 {
-int p = 0;
-/ incremeant up to when the last character is NULL, \0 /
-while (*(s + p) != 0)
+int longi = 0;
+while (*s != '\0')
 {
-p++;
+longi++;
+s++;
 }
-return (p);
+return (longi);
 }
