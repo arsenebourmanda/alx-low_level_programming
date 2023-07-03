03cf2bbc0a37e380dc6edcc65af30f64128b0d0f
koumnomani bourmanda <arsenebourmanda@gmail.com>
2023-07-03 16:32:53 -0700

    creates 3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -7,15 +7,6 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-#include "main.h"
-/**
- * _strspn - Entry point
- * @s: input
- * @accept: input
- * Return: Always 0 (Success)
- */
-unsigned int _strspn(char *s, char *accept)
-{
 	unsigned int n = 0;
 	int r;
 
@@ -34,5 +25,4 @@ unsigned int _strspn(char *s, char *accept)
 		s++;
 	}
 	return (n);
-}
 }
