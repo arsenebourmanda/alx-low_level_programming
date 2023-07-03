1776fdcd3d8a04171db252cbe8169ee0507d47c9
koumnomani bourmanda <arsenebourmanda@gmail.com>
2023-07-03 15:59:29 -0700

    creates 1-memcpy.c

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _memcpy - a function that copies memory area
+ * @dest: memory where is stored
+ * @src: memory where is copied
+ * *@n: number of bytes
+ *
+ * Return: copied memory with n byted changed
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+int i, j;
+for (i = 0, j = n; i < j; i++)
+{
+dest[i] = src[i];
+j--;
+}
+return (dest);
+}
