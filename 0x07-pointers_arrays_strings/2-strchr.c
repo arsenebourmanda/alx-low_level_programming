e41001b7e91aa2ae2ef7810fd4f994fd3ed10547
koumnomani bourmanda <arsenebourmanda@gmail.com>
2023-07-03 16:18:48 -0700

    creates 2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strchr - Entry point
+ * @s: input
+ * @c: input
+ * Return: Always 0 (Success)
+ */
+char *_strchr(char *s, char c)
+{
+int a;
+for (a = 0; s[a] >= '\0'; a++)
+{
+if (s[a] == c)
+return (&s[a]);
+}
+}
