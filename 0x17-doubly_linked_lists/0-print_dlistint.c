55115e015ff6f837b5a069ad2da60f6eb2aa62d2
koumnomani bourmanda <arsenebourmanda@gmail.com>
2023-09-14 15:14:03 -0700

    code

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - all elts of dlistint_t are printed
+ *
+ * @h: list's head
+ * Return: total of the node
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	int num;
+	num = 0;
+
+	if (h == NULL)
+		return (num);
+	while (h->prev != NULL)
+		h = h->prev;
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		num++;
+		h = h->next;
+	}
+
+	return (num);
+}
